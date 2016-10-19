4ed94d837c77901aeb9c6e2c52efcc5c098e4e60
Jai Goyal <goyaljai.y14@gmail.com>
2016-10-19 10:08:52 +0530

    updated settings-py file

--- imagepool/settings.py
+++ imagepool/settings.py
@@ -130,6 +130,7 @@ AUTH_USER_MODEL = 'project.SiteUser'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
